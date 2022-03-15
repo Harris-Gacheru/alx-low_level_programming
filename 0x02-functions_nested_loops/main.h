a79a2c889118e3b416614277696def33b3a432bd
Harris-Gacheru <harrisgacheru.hg@gmail.com>
2022-03-15 11:28:34 -0700

    Initial commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -24,4 +24,12 @@ int _abs(int n);
 /* prototype - print last digit */
 int print_last_digit(int);
 
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int, int);
+
+void print_to_98(int n);
+
 #endif
