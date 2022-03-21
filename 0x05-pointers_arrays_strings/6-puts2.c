681c9f718a99f150b50a1e0af1a2054cdce8494f
Harris-Gacheru <harrisgacheru.hg@gmail.com>
2022-03-21 13:30:44 -0700

    Intial commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the len
+ * @s: string
+ * Retur: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - print every second character of string
+ * @str: string to print
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
